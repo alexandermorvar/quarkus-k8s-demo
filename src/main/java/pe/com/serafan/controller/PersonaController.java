9764ab85c1dbaf279cc0d045c94a06efe5d1aced
Christian <cmorales@example.com>
2026-07-23 15:51:03 -0500

    Nueva funcionalidad_03

--- src/main/java/pe/com/serafan/controller/PersonaController.java
+++ src/main/java/pe/com/serafan/controller/PersonaController.java
@@ -89,6 +89,6 @@ public class PersonaController {
     @Path("/version")
     @Produces(MediaType.TEXT_PLAIN)
     public String version() {
-        return "Version 1.0.3";
+        return "Version 1.0.4";
     }
 }
